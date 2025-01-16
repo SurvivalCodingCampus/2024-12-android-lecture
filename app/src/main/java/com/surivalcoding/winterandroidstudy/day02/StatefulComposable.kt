5996edfe2955c26ce0fa03cc473317b02c481cfe
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-01-16 14:05:27 +0900

    feat: Introduce `CounterScreen` composable with state hoisting
    -   Replaced the `Counter` composable with `CounterScreen`.
    -   `CounterScreen` now receives `count` and `onClick` as parameters to implement state hoisting.
    -   Removed `rememberSaveable` and `mutableIntStateOf` from within `CounterScreen`.
    -   Updated the preview to pass in an initial `count` value.
    -   `CounterScreen` receives `count` and `onClick` from the parent composable.
    -   `onClick` lambda was added to handle the event in the parent composable.
    -   The `count` parameter was added to display the current count value.

--- app/src/main/java/com/surivalcoding/winterandroidstudy/day02/StatefulComposable.kt
+++ app/src/main/java/com/surivalcoding/winterandroidstudy/day02/StatefulComposable.kt
@@ -5,10 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -17,17 +13,17 @@ import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun Counter() {
-    // delegated properties
-    var count by rememberSaveable { mutableIntStateOf(0) }
-
+fun CounterScreen(
+    count: Int = 0,
+    onClick: () -> Unit = {}
+) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
         TextButton(
             onClick = {
-                count++
+                onClick()
             }
         ) {
             Text(
@@ -41,5 +37,7 @@ fun Counter() {
 @Preview(showBackground = true)
 @Composable
 private fun CounterPreview() {
-    Counter()
+    CounterScreen(
+        count = 5
+    )
 }
