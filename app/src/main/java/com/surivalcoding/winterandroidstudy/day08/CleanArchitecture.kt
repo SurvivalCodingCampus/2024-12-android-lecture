1071950df9441c43bb42585c13bba54f932566ed
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-01-22 14:48:15 +0900

    클린 아키텍처

---
+++ app/src/main/java/com/surivalcoding/winterandroidstudy/day08/CleanArchitecture.kt
@@ -0,0 +1,14 @@
+package com.surivalcoding.winterandroidstudy.day08
+
+class UseCase {
+
+    fun execute(id: Int) {
+
+    }
+}
+
+fun main() {
+    val useCase = UseCase()
+
+    useCase.execute(10)
+}
