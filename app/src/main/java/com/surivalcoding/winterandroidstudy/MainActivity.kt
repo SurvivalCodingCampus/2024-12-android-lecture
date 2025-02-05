3849b7e768ee1a62204467492d6f6a9ee4088d58
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-02-05 14:25:56 +0900

    feat: Delegated Properties

--- app/src/main/java/com/surivalcoding/winterandroidstudy/MainActivity.kt
+++ app/src/main/java/com/surivalcoding/winterandroidstudy/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.surivalcoding.winterandroidstudy
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.surivalcoding.winterandroidstudy.day02.Ingredient
 import com.surivalcoding.winterandroidstudy.day02.IngredientItem
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 class MainActivity : ComponentActivity() {
+    private val userSettings by lazy {
+        UserSettings(this)
+    }
+
+
     var count = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         count++
         println("count: $count")
         setContent {
@@ -24,6 +35,21 @@ class MainActivity : ComponentActivity() {
                 )
             )
         }
+
+        val pref = getSharedPreferences("user_settings", Context.MODE_PRIVATE)
+
+
+        pref.edit().putString("name", "홍길동").apply()
+
+
+//        println(pref.getString("name", "기본값"))
+
+//        var name : String by PreferenceDelegate()
+//        println(name)
+
+        userSettings.userName = "한석봉"
+
+        println(userSettings.userName)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -44,3 +70,51 @@ class MainActivity : ComponentActivity() {
         // UI 에 복원
     }
 }
+
+
+class PreferenceDelegate(
+    private val preference: SharedPreferences,
+    private val key: String,
+    private val default: String,
+) : ReadWriteProperty<PreferenceModel, String> {
+
+
+    override fun getValue(thisRef: PreferenceModel, property: KProperty<*>): String {
+        return preference.getString(key, default)!!
+    }
+
+    override fun setValue(thisRef: PreferenceModel, property: KProperty<*>, value: String) {
+        preference.edit().putString(key, value).apply()
+    }
+}
+
+interface PreferenceModel {
+    val context: Context
+}
+
+
+class UserSettings(override val context: Context) : PreferenceModel {
+    var userName by PreferenceLoader("")
+    var iconUrl by PreferenceLoader("")
+    var photoUrl by PreferenceLoader("")
+}
+
+class PreferenceLoader(private val default: String) {
+
+    operator fun provideDelegate(
+        thisRef: PreferenceModel,
+        property: KProperty<*>
+    ): ReadWriteProperty<PreferenceModel, String> {
+        return PreferenceDelegate(
+            thisRef.context.getSharedPreferences(
+                thisRef.javaClass.simpleName,
+                Context.MODE_PRIVATE
+            ),
+            property.name,
+            default
+        )
+    }
+}
+
+
+
